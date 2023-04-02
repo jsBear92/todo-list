3a4417f410381e6bec27a67f814f31593bafd9f5
jsBear92 <js104427@gmail.com>
2023-04-02 15:07:54 +0800

    feat: add tailwindcss and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import './App.css';
 
 type Todo = {
   id: number;
@@ -31,24 +30,37 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Enter a new task"
-      />
-      <button onClick={addTodo}>Add</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-            <input type="checkbox" onChange={() => toggleTodo(todo.id)} checked={todo.completed} />
-            {todo.text}
-            <button onClick={() => removeTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="w-full max-w-lg">
+        <h1 className="text-4xl font-bold mb-4">Todo List</h1>
+        <input
+          className="flex w-full py-2 border border-blue-400 rounded-full focus:border-blue-600 focus:outline-none"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+          onClick={addTodo}
+        >Add</button>
+        <ul className="mt-4 space-y-2">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center space-x-2"
+              style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+            >
+              <input type="checkbox" onChange={() => toggleTodo(todo.id)} checked={todo.completed} />
+              <span className="flex grow">{todo.text}</span>
+              <button
+                className="px-2 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none"
+                onClick={() => removeTodo(todo.id)}
+              >Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
